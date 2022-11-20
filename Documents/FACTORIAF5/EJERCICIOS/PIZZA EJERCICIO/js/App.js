39a02531062c2e5a0de067feedb312761fdd93ff
JehiselRuth <jehisel.rrp@gmail.com>
2022-11-21 00:57:02 +0100

    conexion remoto

---
+++ Documents/FACTORIAF5/EJERCICIOS/PIZZA EJERCICIO/js/App.js
@@ -0,0 +1,35 @@
+/*Colocamos un bowl 
+Añadir 400 gramos de harina de trigo
+Añadir 200 ml de agua tibia
+Añadir 2 cucharadas de aceite de oliva
+Añadir 15 gramos de levadura fresca
+Añadir 10 gr. de sal
+Mezclar todo
+Dejar reposar 20 min. en cámara cerrada para que fermente
+Alisar hasta darle un grosor de 0,5 cm y forma redonda con un rodillo de cocina
+Pinchar la masa
+Añadir 200 ml de tomate triturado repartido por toda la base 
+Añadir 150 gr jamón york troceado repartido por toda la base
+Añadir 150 gr queso rallado repartido por toda la base
+Añadir 15 gr de orégano seco repartido por toda la base
+Introducir al horno 20 min a 180 º con calor por arriba y por abajo
+Sacamos del horno.*/
+
+let bowl = "bowl";
+let GrSalt = 10;
+let flour = "harina"; 
+let GrFlour = 400;
+let cheese = "cheese"
+let water = "water"
+let oliveoil = "oliveoil"
+let yeast = "yeast"
+
+let grams
+
+function MakePizza() {
+    console.log("Colocamos un", bowl);
+    console.log("añadir",GrFlour, )
+
+
+    
+}
